074a2043a7d86d5ab1fb4f473e9ac35909af3dee
corneliu.iancu29 <corneliu.iancu29@gmail.com>
2016-10-24 05:47:18 -0400

    move compiled ts to dist folder

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-@NgModule({
+@NgModule({ 
   imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
